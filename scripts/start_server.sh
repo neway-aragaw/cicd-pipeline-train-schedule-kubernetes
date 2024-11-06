6479af0c28bcb808fb68feb371706475f43e1bee
Neway Aragaw <109878276+neway-aragaw@users.noreply.github.com>
2024-11-06 03:16:35 -0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,7 @@
 # Navigate to the app directory
 cd /home/ec2-user/trainSchedule
 # Stop any running instances of the app
-pm2 stop myapp || true
+pm2 stop trainSchedule || true
 # Install dependencies (if necessary)
 npm install
 # Start the application
