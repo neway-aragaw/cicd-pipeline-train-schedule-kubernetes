2c003cc0a11ab4aef4f5e201a00db0b312e75a6c
Neway Aragaw <109878276+neway-aragaw@users.noreply.github.com>
2024-11-06 11:27:26 -0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-# Navigate to the app directory
-cd /home/ec2-user/cicd-pipeline-train-schedule-kubernetes
-# Stop any running instances of the app
-pm2 stop cicd-pipeline-train-schedule-kubernetes || true
-# Install dependencies (if necessary)
-npm install
-# Start the application
-pm2 start app.js --name cicd-pipeline-train-schedule-kubernetes
+service httpd start
+
+
+
+
+# #!/bin/bash
+# # Navigate to the app directory
+# cd /home/ec2-user/cicd-pipeline-train-schedule-kubernetes
+# # Stop any running instances of the app
+# pm2 stop cicd-pipeline-train-schedule-kubernetes || true
+# # Install dependencies (if necessary)
+# npm install
+# # Start the application
+# pm2 start app.js --name cicd-pipeline-train-schedule-kubernetes
